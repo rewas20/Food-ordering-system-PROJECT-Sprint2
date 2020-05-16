ca6385fcbf516ddd6e4d186465713e1e6a766c23
ibrahimshawki <Ibrahimshawki646@gmail.com>
2020-05-16 19:34:50 +0200

    CategorysController

---
+++ Controllers/CategorysController.cs
@@ -0,0 +1,22 @@
+﻿using Food_ordering_system_PROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Food_ordering_system_PROJECT.Controllers
+{
+    public class CategorysController : Controller
+    {
+        private StoreEntities db = new StoreEntities();
+
+
+
+        // GET: Categories
+        public ActionResult List()
+        {
+            return View(db.Categories.ToList());
+        }
+    }
+}
