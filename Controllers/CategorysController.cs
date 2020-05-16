d33a90791ebbce46c34c9ebf13eb6376c73cfb85
moha99ed <alhanafy.mohamed2030@gmail.com>
2020-05-17 00:54:30 +0200

    create controller category and add update function

--- Controllers/CategorysController.cs
+++ Controllers/CategorysController.cs
@@ -1,7 +1,9 @@
 ﻿using Food_ordering_system_PROJECT.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +19,34 @@ namespace Food_ordering_system_PROJECT.Controllers
         public ActionResult List()
         {
             return View(db.Categories.ToList());
+        }
+        // GET: Update
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST:Updates
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update([Bind(Include = "id,name,number_of_product")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(category);
         }
     }
 }
