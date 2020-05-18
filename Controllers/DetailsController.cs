64971e83cb3e8fa42fa27b52f1e5ccff623c21d3
Ranyaadel <raniaadel9222gmail.com>
2020-05-17 20:15:50 -0700

    Added Details controller

---
+++ Controllers/DetailsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Food_ordering_system_PROJECT.Models;
+
+namespace Food_ordering_system_PROJECT.Controllers
+{
+    public class DetailsController : Controller
+    {
+
+        // GET: Details
+        private StoreEntities db = new StoreEntities();
+       
+        public ActionResult Details(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product != null)
+            {
+                ViewBag.Product = product;
+                return View();
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
