9d17c204a9b060f44f658c7e64f812dbac0d12d1
rewas safwat <rewasssafwat51@gmail.com>
2020-05-22 02:13:08 +0200

    Add View Delete product and test code

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,105 @@
+﻿using Food_ordering_system_PROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Food_ordering_system_PROJECT.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private StoreEntities db = new StoreEntities();
+
+
+
+        // GET: Categories
+        public ActionResult List()
+        {
+            var categories = View(db.Categories.Where(c => c.name != null).ToList());
+            if (categories != null)
+                return View(categories);
+            else
+                return RedirectToAction("Create");
+        }
+        // GET: Update
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST:Updates
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update([Bind(Include = "id,name,number_of_product")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(category);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,number_of_products")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            return View(category);
+        }
+
+        // GET:Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST:Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+    }
+}
