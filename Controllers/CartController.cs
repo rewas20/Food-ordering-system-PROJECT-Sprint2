863c4a1c90c82510815a1e9e466f933097f13e55
Nourhan52 <noramgd99@gmail.com>
2020-05-27 08:35:46 +0200

    Add to cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Food_ordering_system_PROJECT.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +16,20 @@ namespace Food_ordering_system_PROJECT.Controllers
         {
             return View();
         }
+    }
+    public ActionResult Addtocart(int id)
+    {
+        var search = db.Carts.Find(id);
+        if (search == null)
+        {
+            Cart cart = new Cart();
+            cart.product_id = id;
+            cart.added_at = DateTime.Now;
+            db.Carts.Add(cart);
+            db.SaveChanges();
+        }
+
+
+        return RedirectToAction("Index");
     }
 }
